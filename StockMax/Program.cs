081f5a8fa581ef2a00a670017bc7ea8a28403b31
Ifeanyi Ikefuna <iikefuna@parkwayprojects.com>
2023-03-16 20:10:50 +0100

    Add project files.

---
+++ StockMax/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace StockMax
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
+            Console.ReadLine();
+        }
+
+        public static long StockMax(List<int> prices)
+        {
+
+            long res = 0;
+
+            long cap = 0;
+
+            for (int i = prices.Count - 1; i >= 0; i--)
+            {
+                cap = Math.Max(cap, (long)prices[i]);
+                res = res + cap - prices[i];
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,40 @@
+using StockMax;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_For_Loss()
+        {
+            //Arrange
+            List<int> prices = new() { 5, 3, 2 };
+            long expectedResult = 0;
+
+            //Act
+            long result = Program.StockMax(prices);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void Test_For_Profit()
+        {
+            //Arrange
+            List<int> prices = new() { 1, 2, 100 };
+            long expectedResult = 197;
+
+            //Act
+            long result = Program.StockMax(prices);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
